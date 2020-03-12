2f65c7c4addeef628707f5b86fdd706fc7a80670
Hsin-Mao Wu <hsinmao@maoshouse.com>
2020-03-11 22:55:21 -0700

    set logroot.

--- roadnoise/roadnoise.py
+++ roadnoise/roadnoise.py
@@ -12,7 +12,7 @@ ID_PRODUCT = 0x74e3
 def main():
     usb_db_poller = USBDbDevice("USB Db", get_usb_decibel_meter())
     pollers = [usb_db_poller]
-    file_handler = GzipTimedRotatingFileHandler("roadnoise", "h", 1, 7)
+    file_handler = GzipTimedRotatingFileHandler("roadnoise", ".", "h", 1, 7)
     logger = CsvLogger("logger", file_handler)
     reporter = Reporter(pollers, logger, 1)
     reporter.start()
