2d096c5009709a6ab7e28e8e50ae00e22497c049
Hsin-Mao Wu <hsinmao@maoshouse.com>
2020-03-11 20:41:52 -0700

    Update roadnoise main function to create a reporter and start reporting.

--- roadnoise/roadnoise.py
+++ roadnoise/roadnoise.py
@@ -1,17 +1,31 @@
 import hid
 
 from .device.usb_db_device import USBDbDevice
+from .logging.csv_logger import CsvLogger
+from .logging.gzip_timed_rotating_file_handler import GzipTimedRotatingFileHandler
+from .reporter.reporter import Reporter
 
 ID_VENDOR = 0x64bd
 ID_PRODUCT = 0x74e3
 
 
 def main():
-    usb_db_meter = __get_usb_decibel_meter()
-    usb_db_poller = USBDbDevice("USB Db", usb_db_meter)
+    usb_db_poller = USBDbDevice("USB Db", get_usb_decibel_meter())
+    pollers = [usb_db_poller]
+    file_handler = GzipTimedRotatingFileHandler("roadnoise", "h", 1, 7)
+    logger = CsvLogger("logger", file_handler)
+    reporter = Reporter(pollers, logger, 1)
+    reporter.start()
+    try:
+        while True:
+            pass
+    finally:
+        reporter.stop()
+
+    exit(0)
 
 
-def __get_usb_decibel_meter():
+def get_usb_decibel_meter():
     usb_decibel_meter = hid.device()
     usb_decibel_meter.open(ID_VENDOR, ID_PRODUCT)
     assert usb_decibel_meter is not None
