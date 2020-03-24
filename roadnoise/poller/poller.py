6623d3530aaadd24bb0ad3f8f26a13f2d7a42f28
Hsin-Mao Wu <hsinmao@maoshouse.com>
2020-03-23 21:59:35 -0700

    Introduce default polling period of 0.25 seconds as 1 seconds has a timing issue and won't return data.

--- roadnoise/poller/poller.py
+++ roadnoise/poller/poller.py
@@ -1,8 +1,9 @@
+import time
 from concurrent.futures.thread import ThreadPoolExecutor
 
 
 class Poller:
-    def __init__(self, device, period_seconds):
+    def __init__(self, device, period_seconds=0.25):
         self.__threadpool_executor = ThreadPoolExecutor(1)
         self.__device = device
         self.__period_seconds = period_seconds
@@ -24,4 +25,4 @@ class Poller:
         while self.__started:
             self.__value = self.__device.read()
             print("Polled: ", self.__value)
-            # time.sleep(self.__period_seconds)
+            time.sleep(self.__period_seconds)
