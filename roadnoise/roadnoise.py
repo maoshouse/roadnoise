b7e93c6caab3bd1f57085e7fd41cccf06b3f6205
Hsin-Mao Wu <hsinmao@maoshouse.com>
2020-04-13 22:35:30 -0700

    Add usb gps poller.

--- roadnoise/roadnoise.py
+++ roadnoise/roadnoise.py
@@ -1,6 +1,8 @@
 import hid
+import serial
 
 from .device.usb_db_device import USBDbDevice
+from .device.usb_gps_device import USBGpsDevice
 from .logging.dict_logger import DictLogger
 from .logging.gzip_timed_rotating_file_handler import GzipTimedRotatingFileHandler
 from .poller.poller import Poller
@@ -13,7 +15,9 @@ ID_PRODUCT = 0x74e3
 def main():
     usb_db_device = USBDbDevice("USB Db", get_usb_decibel_meter())
     usb_db_poller = Poller(usb_db_device)
-    pollers = [usb_db_poller]
+    usb_gps_device = USBGpsDevice("USB GPS", get_usb_gps())
+    usb_gps_poller = Poller(usb_gps_device)
+    pollers = [usb_db_poller, usb_gps_poller]
     file_handler = GzipTimedRotatingFileHandler("roadnoise", ".", "h", 1, 7)
     logger = DictLogger("logger", file_handler)
     reporter = Reporter(pollers, logger, 1)
@@ -32,3 +36,7 @@ def get_usb_decibel_meter():
     usb_decibel_meter.open(ID_VENDOR, ID_PRODUCT)
     assert usb_decibel_meter is not None
     return usb_decibel_meter
+
+
+def get_usb_gps():
+    return serial.Serial('/dev/ttyUSB0', 4800, timeout=5)
