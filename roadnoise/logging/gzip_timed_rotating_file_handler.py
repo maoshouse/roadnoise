db640c1d0b18af7465083aa85fda8cfe0d3e2807
Hsin-Mao Wu <hsinmao@maoshouse.com>
2020-05-06 18:28:22 -0700

    Implement separate behavior for rollover on compressed or non_compressed specification.

--- roadnoise/logging/gzip_timed_rotating_file_handler.py
+++ roadnoise/logging/gzip_timed_rotating_file_handler.py
@@ -18,24 +18,32 @@ class GzipTimedRotatingFileHandler(TimedRotatingFileHandler):
     # https://medium.com/@rahulraghu94/overriding-pythons-timedrotatingfilehandler-to-compress-your-log-files-iot-c766a4ace240
     def doRollover(self):
         super().doRollover()
-        if self.__compress_on_rollover:
-            self.compress_logs()
+        self.rollover_logs()
 
-    def compress_logs(self):
+    def rollover_logs(self):
         log_dir = dirname(self.baseFilename)
-        files_to_compress = [join(log_dir, file) for file in os.listdir(log_dir) if
+        files_to_rollover = [join(log_dir, file) for file in os.listdir(log_dir) if
                              file.startswith(self.__file_name_prefix) and not file.endswith((".gz", ".log"))]
-        for file in files_to_compress:
+        for file in files_to_rollover:
             if exists(file):
-                with open(file, "rb") as to_compress, gzip.open(self.__get_rollover_path(file), "wb") as compressed:
-                    shutil.copyfileobj(to_compress, compressed)
-            os.remove(file)
+                if self.__compress_on_rollover:
+                    with open(file, "rb") as to_compress, gzip.open(self.__get_rollover_path(file), "wb") as compressed:
+                        shutil.copyfileobj(to_compress, compressed)
+                else:
+                    with open(file, "rb") as to_compress, open(self.__get_rollover_path(file), "wb") as non_compressed:
+                        shutil.copyfileobj(to_compress, non_compressed)
+                os.remove(file)
 
     def __get_log_path(self, file_name, log_root):
         Path(log_root).mkdir(parents=True, exist_ok=True)
         return log_root + "/" + file_name
 
     def __get_rollover_path(self, file):
-        rollover_dir = "{}/compressed".format(self.__log_root)
-        Path(rollover_dir).mkdir(parents=True, exist_ok=True)
-        return "{}/{}.gz".format(rollover_dir, file.split('/')[-1])
+        if self.__compress_on_rollover:
+            rollover_dir = "{}/compressed".format(self.__log_root)
+            Path(rollover_dir).mkdir(parents=True, exist_ok=True)
+            return "{}/{}.gz".format(rollover_dir, file.split('/')[-1])
+        else:
+            rollover_dir = "{}/non_compressed".format(self.__log_root)
+            Path(rollover_dir).mkdir(parents=True, exist_ok=True)
+            return "{}/{}".format(rollover_dir, file.split('/')[-1])
